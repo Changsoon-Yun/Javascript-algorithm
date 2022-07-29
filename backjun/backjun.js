27a541a705fd376cde16edefcf576ffc86b8ba82
Changsoon-YUN <changsoonyun@naver.com>
2022-07-30 04:47:50 +0900

    .

--- backjun/backjun.js
+++ backjun/backjun.js
@@ -1,7 +1,18 @@
+//한 줄 입력
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let num = Number(input);
+
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
+
 //여러 줄 입력
 
 let fs = require('fs');
-let input = fs.readFileSync('../input.txt').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let count = input[0];
 let numbers = [];
