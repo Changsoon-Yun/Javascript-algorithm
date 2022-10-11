f8952268836559303a9b163c68e28332f7ef29fd
Changsoon-YUN <92949174+Changsoon-Yun@users.noreply.github.com>
2022-10-11 22:53:54 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -1,30 +1,21 @@
 let fs = require("fs");
-let input = fs.readFileSync("dev/stdin").toString().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let number1 = input[0];
 let number2 = input[1];
-let line3 = 0;
+let = 0;
 
 let one = number1[0] * 100;
 let ten = number1[1] * 10;
 let hundred = number1[2];
 
-line3 =
-  one * Number(number2[2]) +
-  ten * Number(number2[2]) +
-  hundred * Number(number2[2]);
+let line3 = one * number2[2] + ten * number2[2] + hundred * number2[2];
 
-line4 =
-  one * Number(number2[1]) +
-  ten * Number(number2[1]) +
-  hundred * Number(number2[1]);
+let line4 = one * number2[1] + ten * number2[1] + hundred * number2[1];
 
-line5 =
-  one * Number(number2[0]) +
-  ten * Number(number2[0]) +
-  hundred * Number(number2[0]);
+let line5 = one * number2[0] + ten * number2[0] + hundred * number2[0];
 
-line6 = line3 + line4 * 10 + line5 * 100;
+let line6 = line3 + line4 * 10 + line5 * 100;
 
 console.log(line3);
 console.log(line4);
