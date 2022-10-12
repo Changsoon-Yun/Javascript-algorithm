8be2523fa77305e7884c8c7068614e826058cc45
changsoonyun <changsoonyun@naver.com>
2022-10-12 22:04:11 +0900

    .

--- backjun/test.js
+++ backjun/test.js
@@ -1,20 +1,33 @@
-let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().split('\n');
-let limit = input[0].split(" ")
-let givenCard = input[1].split(" ")
-let answer = []
-function solution(n) {
-  for (let i = 0; i < givenCard.length; i++) {
-    for (let j = i+1; j < givenCard.length; j++) {
-      for (let k = j+1; k < givenCard.length; k++) {
-        if(parseInt(limit[1]) >= parseInt(givenCard[i])+parseInt(givenCard[j])+parseInt(givenCard[k])) {
-          answer.push(parseInt(givenCard[i])+parseInt(givenCard[j])+parseInt(givenCard[k]))
-        }
-      }
-    }
-  }
-  console.log(answer.sort((a,b)=> b-a)[0])
-  return answer.sort((a,b)=> b-a)[0]
+let fs = require("fs");
+let input = fs.readFileSync("input.txt").toString().trim().split("\n");
+let testLength = [];
+let lineNumber = [];
+
+for (let i = 1; i < input.length; i++) {
+  input[i].length === 2 && testLength.push(input[i]);
 }
 
-solution(input)
+for (let i = 1; i < input.length; i++) {
+  for (let j = 0; j < testLength.length; j++) {
+    input[i] === testLength[j] && lineNumber.push(i);
+  }
+}
+
+let answer = [];
+for (let i = 0; i < input[0]; i++) {
+  for (let j = 0; j < testLength[i]; j++) {
+    if (input[lineNumber[i] + j + 1].split(" ").sort((a, b) => b - a)[0] >= 0) {
+      answer.push(
+        input[lineNumber[i] + j + 1].split(" ").sort((a, b) => b - a)[0]
+      );
+    }
+  }
+}
+
+for (let i = 0; i < input[0]; i++) {
+  console.log(
+    answer
+      .slice(i * 3, i * 3 + 3)
+      .reduce((prev, curr) => Number(prev) + Number(curr))
+  );
+}
