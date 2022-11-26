74300994a7eb32089b369a4db169927f882f22b0
Changsoon-YUN <92949174+Changsoon-Yun@users.noreply.github.com>
2022-11-26 22:53:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.93 ms, Memory: 30.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,38 @@
+function solution(new_id) {
+    var answer = '';
+    var reg = /[\{\}\[\]\/?,;:|\)*~`!^\+<>@\#$%&\\\=\(\'\"]/gi;
+    let id = new_id.toLowerCase().replace(reg, '').split('')
+    for (let i = 0; i < id.length; i++) {
+        if(id[i]=== '.' && id[i+1]==='.'){
+            id.splice(i,1)
+            i--
+        }
+    }
+    for (let i = 0; i < id.length; i++) {
+        if(id[0]==='.') {
+            id.shift()
+        }
+        if(id[id.length - 1] === '.') {
+            id.pop()
+        }
+    }
+    if(id.length===0) {
+        id.push('a')
+    }
+    for (let i = 0; i < id.length; i++) {
+        if(id.length >= 15 && i>=15) {
+            id.splice(i,1)
+            i--
+        }
+        if(id[id.length - 1] === '.') {
+            id.pop()
+        }
+        if(id.length<=2) {
+            id.push(id[id.length-1])
+        }
+    }
+    id = id.join('')
+    console.log(id)
+    answer = id
+    return answer;
+}
