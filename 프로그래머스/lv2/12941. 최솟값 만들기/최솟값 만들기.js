dcbcb29bebe20bfab9258b09a1c3f9ae8486d058
Changsoon-YUN <92949174+Changsoon-Yun@users.noreply.github.com>
2022-11-30 22:17:27 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.78 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,17 @@
+function solution(A, B) {
+  var answer = 0;
+
+  A.sort((a, b) => {
+    return a - b;
+  });
+
+  B.sort((a, b) => {
+    return a - b;
+  });
+
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[B.length - 1 - i];
+  }
+
+  return answer;
+}
