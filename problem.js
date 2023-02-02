9e349447a8d1d5faa1e1fb727ca098e7e264f75c
csyun <csyun@creativelab-inc.com>
2023-02-02 14:14:29 +0900

    saame

--- problem.js
+++ problem.js
@@ -1,6 +1,15 @@
-function solution(spell, dic) {
-  let answer = 0;
+function solution(arr1, arr2) {
+  let answer = false;
+  if(arr1.length !== arr2.length) return answer
+  let newArr = arr1.sort((a,b)=>a-b).map((v)=> v*v)
+  arr2.sort((a,b)=>a-b)
+
+  // console.log(newArr)
+
+  answer = JSON.stringify(newArr) === JSON.stringify(arr2)
+  console.log(answer)
+
   return answer;
 }
 
-solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"]);
+solution([1,3,2], [1,4,9]);
