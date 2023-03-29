5c89e2e39cbb6b3b4911b1ecdf6120645f03d1cc
Changsoon-YUN <92949174+Changsoon-Yun@users.noreply.github.com>
2023-03-29 13:31:19 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
@@ -1,10 +1,4 @@
 function solution(m, n) {
-  let answer = 0;
-  if(m === 1 && n === 1) return answer
-  let min = Math.min(...[m,n])
-  let max = Math.max(...[m,n])
 
-  answer = (min-1) + ((max-1) * min)
-
-  return answer;
+  return m*n-1;
 }
