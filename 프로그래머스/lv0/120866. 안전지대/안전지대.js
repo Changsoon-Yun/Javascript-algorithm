de4eadc14498431ed56a3edfbcfebe169277178a
yuncs <92949174+Changsoon-Yun@users.noreply.github.com>
2023-06-15 16:45:25 +0900

    [level 0] Title: 안전지대, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -0,0 +1,56 @@
+function solution(board) {
+    let answer = 0;
+    let location = []
+    const length = board[0].length
+    
+        if(board[0][0] === 1) {
+        return answer =  0
+    }
+
+    board.map((row, i) => {
+        return row.map((item, j) => {
+            if (item !== 0) {
+                location.push([i,j])
+            }
+        })
+    })
+
+    location.map(bomb => {
+        if(bomb[0] === 0) {
+            board[bomb[0] ][bomb[1] - 1] ++
+            board[bomb[0]][bomb[1] + 1] ++
+            board[bomb[0] +1 ][bomb[1] - 1] ++
+            board[bomb[0] +1 ][bomb[1]] ++
+            board[bomb[0] +1 ][bomb[1] + 1] ++
+            return;
+        }
+
+        if(bomb[0] === length-1) {
+            board[bomb[0] -1 ][bomb[1] - 1] ++
+            board[bomb[0] -1 ][bomb[1]] ++
+            board[bomb[0] -1 ][bomb[1] + 1] ++
+            board[bomb[0] ][bomb[1] - 1] ++
+            board[bomb[0]][bomb[1] + 1] ++
+            return
+        }
+        board[bomb[0] -1 ][bomb[1] - 1] ++
+        board[bomb[0] -1 ][bomb[1]] ++
+        board[bomb[0] -1 ][bomb[1] + 1] ++
+        board[bomb[0] ][bomb[1] - 1] ++
+        board[bomb[0]][bomb[1] + 1] ++
+        board[bomb[0] +1 ][bomb[1] - 1] ++
+        board[bomb[0] +1 ][bomb[1]] ++
+        board[bomb[0] +1 ][bomb[1] + 1] ++
+    })
+
+    board.map((row, i) => {
+        return row.map((item, j) => {
+            if (item === 0) {
+                answer ++
+            }
+        })
+    })
+
+    return answer;
+}
+
