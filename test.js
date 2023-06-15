fbc542d02ee69e60b14e313834ef7ae3856755d0
csyun <csyun@creativelab-inc.com>
2023-06-15 16:50:52 +0900

    .

--- test.js
+++ test.js
@@ -1,5 +1,69 @@
-function solution(list,n) {
-  list.sort((a,b)=>a - b)
-  return list.sort((a,b)=> Math.abs(n-b) - Math.abs(n-a)).reverse()
+function solution(board) {
+    let answer = 0;
+    let location = []
+    const length = board[0].length
+
+    if (board[0][0] === 1) {
+        return answer = 0
+    }
+
+    board.map((row, i) => {
+        return row.map((item, j) => {
+            if (item !== 0) {
+                location.push([i, j])
+            }
+        })
+    })
+
+
+    location.map(bomb => {
+
+
+        if (bomb[0] === 0) {
+            board[bomb[0]][bomb[1] - 1]++
+            board[bomb[0]][bomb[1] + 1]++
+
+            board[bomb[0] + 1][bomb[1] - 1]++
+            board[bomb[0] + 1][bomb[1]]++
+            board[bomb[0] + 1][bomb[1] + 1]++
+            return;
+        }
+
+        if (bomb[0] === length - 1) {
+            board[bomb[0] - 1][bomb[1] - 1]++
+            board[bomb[0] - 1][bomb[1]]++
+            board[bomb[0] - 1][bomb[1] + 1]++
+
+            board[bomb[0]][bomb[1] - 1]++
+            board[bomb[0]][bomb[1] + 1]++
+
+            return
+        }
+
+
+        board[bomb[0] - 1][bomb[1] - 1]++
+        board[bomb[0] - 1][bomb[1]]++
+        board[bomb[0] - 1][bomb[1] + 1]++
+
+        board[bomb[0]][bomb[1] - 1]++
+        board[bomb[0]][bomb[1] + 1]++
+
+        board[bomb[0] + 1][bomb[1] - 1]++
+        board[bomb[0] + 1][bomb[1]]++
+        board[bomb[0] + 1][bomb[1] + 1]++
+
+    })
+
+    board.map((row, i) => {
+        return row.map((item, j) => {
+            if (item === 0) {
+                answer++
+            }
+        })
+    })
+
+
+    return answer;
 }
-solution([10,9,8,7,6,5,4,3,2,1,11],6);
+
+solution([[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0]])
