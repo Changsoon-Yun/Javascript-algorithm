e76e88a8ce4f7750ca203ae34d5ac3cbb28ac022
csyun <csyun@creativelab-inc.com>
2023-06-20 10:07:57 +0900

    Refactor function to sort array and return first element in present tense.

--- lecture/최솟값 구하기.js
+++ lecture/최솟값 구하기.js
@@ -1,10 +1,14 @@
 function solution(arr){
-    let answer, min=Number.MAX_SAFE_INTEGER;
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]<min) min=arr[i];
-    }
-    answer=min;
-    return answer;
+    // let answer, min=Number.MAX_SAFE_INTEGER;
+    // for(let i=0; i<arr.length; i++){
+    //     if(arr[i]<min) min=arr[i];
+    // }
+    // answer=min;
+    // return answer;
+
+    arr.sort((a,b)=> (a-b))
+
+    return arr[0]
 
 }
 
