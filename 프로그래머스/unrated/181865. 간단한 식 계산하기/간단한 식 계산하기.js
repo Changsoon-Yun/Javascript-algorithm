f32e10d7b93bcb2fff1330fc6ab4df7fb9a50520
yuncs <92949174+Changsoon-Yun@users.noreply.github.com>
2023-07-19 10:47:29 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+    let a = binomial.split(" ")
+    if( a[1] === "+") {
+        return +a[0] + +a[2]
+    } else if( a[1] === "-") {
+        return a[0] - a[2]
+    } if( a[1] === "*") {
+        return a[0] * a[2]
+    } 
+}
