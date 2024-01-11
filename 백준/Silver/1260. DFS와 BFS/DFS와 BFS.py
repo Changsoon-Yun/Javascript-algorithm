49179794e1ebabeb4912180ab860d485f069d9e9
csyun <92949174+Changsoon-Yun@users.noreply.github.com>
2024-01-12 03:08:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 160 ms, Memory: 111444 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,5 +1,6 @@
 import sys
 
+
 dot, line, start = map(int, input().split())
 
 graph = [[] for i in range(dot + 1)]
@@ -13,9 +14,9 @@ visited = [0] * (dot + 1)
 
 dfs_answer = []
 
-
 for i, line in enumerate(graph):
     line.sort()
+
 
 def dfs(n):
     print(n, end=" ")
@@ -38,9 +39,11 @@ def bfs():
         cur = q.pop(0)
         print(cur, end=" ")
         for num in graph[cur]:
-            if not visited[num] and graph[num]:
+            if not visited[num]:
                 visited[num] = 1
                 q.append(num)
 
 
 bfs()
+
+
