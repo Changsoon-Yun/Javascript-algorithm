1553df3d1d505c832bba41d00a3f85bf1364fad0
csyun <92949174+Changsoon-Yun@users.noreply.github.com>
2024-05-12 17:25:52 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    var answer = 0;
+    if(n % 2 !== 0) {
+        for(let i = n; i>0; i--) {
+            if(i % 2 !==0) {
+            answer += i    
+            }
+            
+        }
+    } else {
+        for(let i = n; i>0; i--) {
+            if(i % 2 ===0) {
+                answer += i*i    
+            }   
+            
+        }
+    }
+    return answer;
+}
